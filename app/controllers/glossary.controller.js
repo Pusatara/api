d94a5d17053a981758d57f485e21f25e5780a9da
Tristan <29161072+trisbman@users.noreply.github.com>
2023-12-16 13:36:05 +0700

    feat: glossary localization

--- app/controllers/glossary.controller.js
+++ app/controllers/glossary.controller.js
@@ -1,9 +1,39 @@
 const g = require('../data/g.json');
+const g_id = require('../data/g_id.json');
+
+const detectLang = (req) => {
+  let lang = req.headers['accept-language'];
+  if (lang) {
+    lang = lang.split(',')[0];
+    if (lang === 'id') {
+      return 'id';
+    }
+  }
+  return 'en';
+}
 
 exports.getGlossary = (req, res) => {
-  res.json(g);
+  switch(detectLang(req)) {
+    case 'en':
+      res.json(g);
+      break;
+    case 'id':
+      res.json(g_id);
+      break;
+    default:
+      res.json(g);
+  }
 };
 
 exports.getGlossaryById = (req, res) => {
-  res.json(g[req.params.id - 1]);
+  switch(detectLang(req)) {
+    case 'en':
+      res.json(g[req.params.id - 1]);
+      break;
+    case 'id':
+      res.json(g_id[req.params.id - 1]);
+      break;
+    default:
+      res.json(g[req.params.id - 1]);
+  }
 };
